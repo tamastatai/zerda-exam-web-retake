220ca4763bd735e859ae668f5d9b077aa64d3949
Tamas Tatai <tataitamas@gmail.com>
2017-01-23 10:39:18 +0100

    post method added

--- client/client.js
+++ client/client.js
@@ -6,4 +6,15 @@ var button = document.querySelector('button');
 
 button.addEventListener('click', function () {
   button.innerHTML = "Loading";
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'http://localhost:3000/turnomatic');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  
+  var feedback = {
+    'admin': document.querySelector('#admin').value,
+    'tech': document.querySelector('#tech').value,
+    'hoodie': document.querySelector('#hoodie').value
+  }
+  xhr.send(JSON.stringify(feedback));
 });
